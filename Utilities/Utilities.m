fdc1be27ef8b77bba07893ec83f04f72f313f60b
SAFedorov <sergey.fedorov@epfl.ch>
2018-04-28 17:09:26 +0200

    Added main "Utilities" file

---
+++ Utilities/Utilities.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+(*Main file that loads all the package components, which can be also loaded separately*)
+BeginPackage["Utilities`"]
+
+(*The base folder named "Utilities" should be on the Mathematica $Path*)
+(*Get[FileNameJoin[{"Utilities","DataAnalysis.m"}]]*)
+Get[FileNameJoin[{"Utilities","Plotting.m"}]]
+Get[FileNameJoin[{"Utilities","Fourier.m"}]]
+(*Get[FileNameJoin[{"Utilities","Compatibility.m"}]]*)
+
+EndPackage[]
